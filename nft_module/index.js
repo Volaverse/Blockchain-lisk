87f72f27c878928eb091175a18a0ca8cdc9ee452
ChintanChawla <dakshchintan@gmail.com>
2022-10-26 17:56:40 +0530

    adding sell nft

---
+++ nft_module/index.js
@@ -0,0 +1,37 @@
+const { BaseModule } = require("lisk-sdk");
+const { getAllNFTTokensAsJSON } = require("./nft");
+
+const CreateNFTAsset = require("./transactions/create_nft_asset");
+const PurchaseNFTAsset = require("./transactions/purchase_nft_asset");
+const TransferNFTAsset = require("./transactions/transfer_nft_asset");
+const TransferTestNFTAsset = require("./transactions/transfer_test_nft_asset");
+const SellNFTAsset = require("./transactions/sell_nft_asset");
+
+// Extend base module to implement your custom module
+class NFTModule extends BaseModule {
+  name = "nft";
+  id = 1024;
+  accountSchema = {
+    type: "object",
+    required: ["ownNFTs"],
+    properties: {
+      ownNFTs: {
+        type: "array",
+        fieldNumber: 1,
+        items: {
+          dataType: "bytes",
+        },
+      },
+    },
+    default: {
+      ownNFTs: [],
+    },
+  };
+  transactionAssets = [new CreateNFTAsset(), new PurchaseNFTAsset(), new TransferNFTAsset(),new TransferTestNFTAsset(),new SellNFTAsset()];
+  actions = {
+    // get all the registered NFT tokens from blockchain
+    getAllNFTTokens: async () => getAllNFTTokensAsJSON(this._dataAccess),
+  };
+}
+
+module.exports = { NFTModule };
---
+++ nft_module/transactions/sell_nft_asset.js
@@ -0,0 +1,58 @@
+const { BaseAsset } = require("lisk-sdk");
+const { getAllNFTTokens, setAllNFTTokens } = require("../nft");
+
+// 1.extend base asset to implement your custom asset
+class SellNFTAsset extends BaseAsset {
+  // 2.define unique asset name and id
+  name = "sellNFT";
+  id = 3;
+  // 3.define asset schema for serialization
+  schema = {
+    $id: "lisk/nft/sell",
+    type: "object",
+    required: ["nftId", "minPurchaseMargin"],
+    properties: {
+      nftId: {
+        dataType: "bytes",
+        fieldNumber: 1,
+      },
+      minPurchaseMargin: {
+        dataType: "uint32",
+        fieldNumber: 2,
+      },
+      name: {
+        dataType: "string",
+        fieldNumber: 3,
+      },
+    },
+  };
+
+  async apply({ asset, stateStore, reducerHandler, transaction }) {
+    const nftTokens = await getAllNFTTokens(stateStore);
+    const nftTokenIndex = nftTokens.findIndex((t) => t.id.equals(asset.nftId));
+
+    // 4.verify if the nft exists
+    if (nftTokenIndex < 0) {
+      throw new Error("Token id not found");
+    }
+    if (asset.minPurchaseMargin < 0 || asset.minPurchaseMargin > 100) {
+      throw new Error("The NFT minimum purchase value needs to be between 0 and 100.");
+    }
+    const token = nftTokens[nftTokenIndex];
+    const tokenOwnerAddress = token.ownerAddress;
+    const senderAddress = transaction.senderAddress;
+    // 5.verify that the sender owns the nft
+
+    if (!tokenOwnerAddress.equals(senderAddress)) {
+      throw new Error("An NFT can only be sold by the owner of the NFT.");
+    }
+
+    // setting the minimum sell value
+
+    token.minPurchaseMargin = asset.minPurchaseMargin;
+    nftTokens[nftTokenIndex] = token;
+    await setAllNFTTokens(stateStore, nftTokens);
+  }
+}
+
+module.exports = SellNFTAsset;
